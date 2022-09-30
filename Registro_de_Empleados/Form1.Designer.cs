28c1ca9d3dd5f0e93c7dd72bbfe594479adae268
Jhoni <isnp111022@ugb.edu.sv>
2022-09-30 07:59:09 -0600

    Registro_de_Empleados

---
+++ Registro_de_Empleados/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Registro_de_Empleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtAFP = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lbMostrar = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbDUI = new System.Windows.Forms.Label();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DUI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "AFP";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(105, 33);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.Location = new System.Drawing.Point(105, 73);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(100, 20);
+            this.txtDUI.TabIndex = 5;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(105, 108);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 6;
+            // 
+            // txtAFP
+            // 
+            this.txtAFP.Location = new System.Drawing.Point(105, 144);
+            this.txtAFP.Name = "txtAFP";
+            this.txtAFP.Size = new System.Drawing.Size(100, 20);
+            this.txtAFP.TabIndex = 7;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(67, 189);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // lbMostrar
+            // 
+            this.lbMostrar.AutoSize = true;
+            this.lbMostrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbMostrar.Location = new System.Drawing.Point(277, 108);
+            this.lbMostrar.Name = "lbMostrar";
+            this.lbMostrar.Size = new System.Drawing.Size(0, 13);
+            this.lbMostrar.TabIndex = 9;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(206, 189);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(117, 23);
+            this.btnInsertar.TabIndex = 10;
+            this.btnInsertar.Text = "Insertar registro";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(391, 189);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 11;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(419, 75);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(35, 13);
+            this.lbNombre.TabIndex = 12;
+            this.lbNombre.Text = "label5";
+            // 
+            // lbDUI
+            // 
+            this.lbDUI.AutoSize = true;
+            this.lbDUI.Location = new System.Drawing.Point(419, 108);
+            this.lbDUI.Name = "lbDUI";
+            this.lbDUI.Size = new System.Drawing.Size(35, 13);
+            this.lbDUI.TabIndex = 13;
+            this.lbDUI.Text = "label6";
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(391, 163);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(100, 20);
+            this.txtCod.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 280);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.lbDUI);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.lbMostrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtAFP);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de empleados";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtAFP;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label lbMostrar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Label lbDUI;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TextBox txtCod;
+    }
+}
+
---
+++ Registro_de_Empleados/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Registro_de_Empleados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Empleados Empleado = new Empleados();
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "")
+            {
+                errorProvider1.SetError(txtNombre, "No ingreso el nombre"); txtNombre.Focus(); return;
+            }
+
+            errorProvider1.SetError(txtNombre, "");
+
+
+            if (txtDUI.Text == "")
+            {
+                errorProvider1.SetError(txtDUI, "No ingreso el DUI");
+                txtNombre.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtNombre, "");
+
+
+
+
+
+            if (txtNombre.Text == "")
+            {
+                errorProvider1.SetError(txtNombre, "No ingresó el nombre");
+                txtNombre.Focus();
+                return;
+            }
+
+            errorProvider1.SetError(txtNombre, "");
+
+            if (txtDUI.Text == "")
+            {
+                errorProvider1.SetError(txtDUI, "No ingresó el DUI");
+                txtNombre.Focus();
+                return;
+            }
+
+            errorProvider1.SetError(txtNombre, "");
+
+
+            double Salario;
+
+            if (!double.TryParse(txtSalario.Text, out Salario))
+            {
+                errorProvider1.SetError(txtSalario, "No ingresó el salario de forma correcta"); txtSalario.Focus(); return;
+            }
+
+
+
+            Empleado.Nombre = txtNombre.Text;
+            Empleado.Dui = txtDUI.Text;
+            Empleado.Salario = Convert.ToDouble(txtSalario.Text);
+            txtAFP.Text = Empleado.AFP(Empleado.Salario).ToString();
+            lbMostrar.Text = "¡Registro guardado!";
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30");
+
+            conexion.Open();
+
+             string nombre = txtNombre.Text;
+             string dui = txtDUI.Text; 
+             double salario =Double.Parse( txtSalario.Text); 
+             double afp =Double.Parse( txtAFP.Text);
+
+            string cadena = "insert into [Empleados] (Nombre, DUI, Salario, AFP) values ('" + nombre + "','" + dui + "','" + salario + "','" + afp + "')";
+
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+
+            comando.ExecuteNonQuery();
+
+            MessageBox.Show("Los datos se guardaron correctamente");
+            conexion.Close();
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30"); 
+            
+            conexion.Open();
+
+            string cod = txtCod.Text; string cadena = "select Nombre, DUI from Empleados where Id=" + cod; SqlCommand comando = new SqlCommand(cadena, conexion); SqlDataReader registro = comando.ExecuteReader(); 
+            if (registro.Read())
+            {
+                lbNombre.Text = registro["Nombre"].ToString(); lbDUI.Text = registro["DUI"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("No existe un Empleado con el código ingresado"); conexion.Close();
+            }
+               
+        }
+    }
+}
